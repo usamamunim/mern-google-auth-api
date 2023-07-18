4482cf4b6304b00ebce0e2b97200fa96a0359a63
usamamunim <usama.munim48@gmail.com>
2023-07-18 15:24:17 +0500

    Code clean up

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,23 +26,17 @@ const UserController = {
     try {
       const { email, password } = req.body;
       console.log(req.body);
-      console.log('check 0');
 
       const user = await User.findOne({ email });
-      console.log('check 1');
-      console.log(user);
       if (user) return res.status(404).json({ msg: 'User already exsists' });
-      console.log('check 2');
 
       const hashedPass = await bcrypt.hash(password, 12);
       const newUser = {
         email,
         password: hashedPass,
       };
-      console.log('check 3');
 
       const createdUser = await User.create(newUser);
-      console.log('check 4');
 
       const token = jwt.sign(
         { userId: createdUser },
